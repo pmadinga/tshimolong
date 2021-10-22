88367468aadc5d50ee185804828d2a600e1fa0e7
Phiwa <phiwen2@gmail.com>
2021-10-22 11:40:28 +0200

    Initialize survey page with a form

--- src/views/pages/Survey.js
+++ src/views/pages/Survey.js
@@ -1,9 +1,48 @@
 
+import { Container, Form, Button } from "react-bootstrap";
+
 
 const Survey = () => {
     return (
-        <div className="survey">
-            <h1>Survey</h1>
+        <div className="survey pt-4">
+            <Container>
+                <Form>
+                    <h1>Personal Details</h1>
+                    <div className="personal-details">
+                        <Form.Group>
+                            <Form.Label>
+                                Surname
+                                <Form.Control type="text"/>
+                            </Form.Label>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>
+                                First Names
+                                <Form.Control type="text"/>
+                            </Form.Label>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>
+                                Contact Number
+                                <Form.Control type="text"/>
+                            </Form.Label>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>
+                                Date
+                                <Form.Control type="date"/>
+                            </Form.Label>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>
+                                Age
+                                <Form.Control className="age-input" type="number" min="5" max="120"/>
+                            </Form.Label>
+                        </Form.Group>
+                    </div>
+                    <Button type="submit">Submit</Button>
+                </Form>
+            </Container> 
         </div>
     );
 }
