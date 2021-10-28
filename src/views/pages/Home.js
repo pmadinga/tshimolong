9264c726b4218d5622498394973bf6e71fe75a8f
Phiwa <phiwen2@gmail.com>
2021-10-28 16:58:58 +0200

    Improved responsive layout + removed build bundle from repo

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -6,11 +6,11 @@ const Home = () =>{
             <Container className="center">
                 
                 <Row className="links">
-                    <Col sm="12" md="6" lg="6" >
-                        <Nav.Link href="/survey" className="fill-survey">Fill out survey</Nav.Link>
+                    <Col sm="12" md="6" lg="6" className="m-auto">
+                        <Nav.Link href="/survey" className="fill-survey m-auto my-2">Fill out survey</Nav.Link>
                     </Col>
                     <Col sm="12" md="6" lg="6">
-                        <Nav.Link href="/results" className="view-survey ">View Survey results</Nav.Link>
+                        <Nav.Link href="/results" className="view-survey m-auto my-2">View Survey results</Nav.Link>
                     </Col>
                 </Row>
             </Container>
